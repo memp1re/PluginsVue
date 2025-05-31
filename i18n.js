99aaf393adbf516aa4f961c3fdc6a7a9511eeaab
memp1re <zhemchugov.2004@mail.ru>
2025-05-31 14:20:27 +0700

    i18n.js

--- i18n.js
+++ i18n.js
@@ -1,8 +1,12 @@
-export default {
+// i18n.js
+window.i18nPlugin = {
     install(app, options) {
+        // Метод перевода
         app.config.globalProperties.$translate = (key) => {
-            return key.split('.').reduce((o, i) => o?.[i], options)
+            return key.split('.').reduce((o, i) => o?.[i], options) ?? '[перевод не найден]'
         }
+        
+        // Для inject
         app.provide('i18n', options)
     }
 }
